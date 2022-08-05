b0005ce2cf18ea0904f013d28302236297084bfe
moorestephen <97118459+moorestephen@users.noreply.github.com>
2022-08-05 15:30:55 -0600

    Mostly functional table now that can have values added to it. Still need to sort out validation of entries as currently entering anything weird will throw a mistake.

--- src/module-info.java
+++ src/module-info.java
@@ -4,5 +4,5 @@ module StephenMooreFinalProject {
 	requires javafx.base;
 	requires javafx.graphics;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
 }
